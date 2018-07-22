5f8bc5370adc5ba9fbeadc7c9587380554115c94
Simon Andrews <simon.andrews212@gmail.com>
2018-07-22 17:02:40 -0400

    Add command line arguments, help info, move stuff into functions

--- gph4-config.sh
+++ gph4-config.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
 
+OPTIND=1
+camera_ssids=""
+verbose=0
+
 # Performs a GET request with curl, only shows output if there are errors (HTTP
 # errors don't count as "errors" here.
 function quiet_get {
 	curl --silent --show-error --output /dev/null $1
 }
 
-# Detect what network manager software is being used On Ubuntu (and most other
+# Shows help information
+function show_help {
+	echo "gph4-config"
+	echo "==========="
+	echo " -c [SSIDs]    SSIDs for the GoPro cameras you want to configure"
+	echo " -v            Enable verbose logging"
+}
+
+# Detect what network manager software is being used. On Ubuntu (and most other
 # Linux distros) this will be NetworkManager (nmcli). On Mac, this will be
 # networksetup.
-
 networkmanager="unset"
 nm_candidates=( "nmcli" )
+function detect_network_manager {
+	for candidate in "${nm_candidates[@]}"; do
+		which $candidate > /dev/null
+		if [ $? -eq 0 ]; then # check which exit code was 0 (command was found)
+			networkmanager=$candidate
+			break
+		fi
+	done
 
-for candidate in "${nm_candidates[@]}"; do
-	which $candidate > /dev/null
-	if [ $? -eq 0 ]; then # check which exit code was 0 (command was found)
-		networkmanager=$candidate
-		break
+	if [ $networkmanager = "unset" ]; then
+		echo "No supported network managers detected!"
+		echo "Supported network managers are:"
+		echo $nm_candidates
+		exit 1
+	else
+		if [ verbose != 0 ]; then
+			echo "Using $networkmanager as network manager"
+		fi
 	fi
+}
+
+while getopts "h?vc:" opt; do
+	case "$opt" in
+		h) show_help; exit 0;;
+		c) camera_ssids=$OPTARG;;
+		v) verbose=1;;
+	esac
 done
 
-if [ $networkmanager = "unset" ]; then
-	echo "No supported network managers detected!"
-	echo "Supported network managers are:"
-	echo $nm_candidates
-	exit 1
-else
-	echo "Using $networkmanager as network manager"
-fi
+echo $camera_ssids
